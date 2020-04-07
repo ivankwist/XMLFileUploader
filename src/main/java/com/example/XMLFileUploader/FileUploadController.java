34d6d4b5b36d7dc40e6f0bcc5cf305d4e91c3f5c
Ivan Kwist <ivankwist@MacBook-de-Ivan.local>
2020-04-07 01:01:45 -0300

    Initial commit

---
+++ src/main/java/com/example/XMLFileUploader/FileUploadController.java
@@ -0,0 +1,35 @@
+package com.example.XMLFileUploader;
+
+import java.util.Date;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.commons.io.FilenameUtils;
+
+@RestController
+public class FileUploadController {
+
+    @PostMapping("/")
+    public ResponseEntity<UploadResponse> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        UploadResponse response = new UploadResponse(file.getOriginalFilename(), new Date().toString(), file.getSize());
+
+        String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+        String mimeType = file.getContentType();
+
+        return new ResponseEntity<UploadResponse>(response, getHttpStatus(extension, mimeType));
+    }
+
+    private HttpStatus getHttpStatus(String extension, String mimeType){
+        HttpStatus status = HttpStatus.NOT_ACCEPTABLE;;
+
+        if(extension.equals("xml") && mimeType.equals("application/xml")){
+            status = HttpStatus.OK;
+        }
+
+        return status;
+    }
+
+}
---
+++ src/main/java/com/example/XMLFileUploader/XmlFileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.XMLFileUploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlFileUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlFileUploaderApplication.class, args);
+	}
+
+}
